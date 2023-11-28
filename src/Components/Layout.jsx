64bb2e7290365d873e2416b0009cf1784388401b
unknown <h.elshwwam123@gmailcom>
2023-11-28 17:08:44 +0200

    all done yet

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
-import s from "../images/Vector 2.png";
-import Nav from "./Nav";
 
 const Layout = () => {
   return (
     <div>
-      <Nav />
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,68 +1,44 @@
-import { Link, useLocation } from "react-router-dom";
+import { faBarsStaggered, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  let location = useLocation();
+  const [hidden, setHidden] = useState(false);
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.onresize = function () {
+      setWidth(window.innerWidth);
+      console.log(width);
+      width >= 992 ? setHidden(false) : null;
+    };
+  }, [width]);
 
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap relative z-50 w-full bg-white text-sm py-4 dark:bg-gray-800 shadow-sm container">
-      <nav
-        className="max-w-[85rem] w-full mx-auto lg:flex lg:items-center lg:justify-between"
-        aria-label="Global"
-      >
-        <div className="flex items-center justify-between">
-          <a
-            className="inline-flex items-center gap-x-2 text-4xl font-semibold dark:text-white text-[--main-color]"
-            href="#"
-          >
-            Brand
-          </a>
-          <div className="lg:hidden">
+    <div className="relative">
+      <div className="border-b py-5 bg-white relative z-30">
+        <div className="flex justify-between items-center container">
+          <div>
+            <a className="text-4xl font-bold">Brand</a>
+          </div>
+          <div className="lg:hidden block">
             <button
               type="button"
-              className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-gray-700 dark:text-white dark:hover:bg-white/10 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-              data-hs-collapse="#navbar-image-and-text-2"
-              aria-controls="navbar-image-and-text-2"
-              aria-label="Toggle navigation"
+              onClick={() => {
+                setHidden(!hidden);
+              }}
+              className="flex justify-center items-center w-[46px] h-[46px] text-white bg-[--main-color] hover:bg-[--hover-color] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-4 text-center me-2 dark:focus:ring-blue-800"
             >
-              <svg
-                className="hs-collapse-open:hidden flex-shrink-0 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1={3} x2={21} y1={6} y2={6} />
-                <line x1={3} x2={21} y1={12} y2={12} />
-                <line x1={3} x2={21} y1={18} y2={18} />
-              </svg>
-              <svg
-                className="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
+              {hidden ? (
+                <FontAwesomeIcon icon={faXmark} size="xl" />
+              ) : (
+                <FontAwesomeIcon icon={faBarsStaggered} size="lg" />
+              )}
             </button>
           </div>
-        </div>
-        <div
-          id="navbar-image-and-text-2"
-          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow lg:block"
-        >
-          <div className="flex flex-col gap-5 lg:gap-14 mt-5 lg:flex-row lg:items-center lg:justify-end lg:mt-0 lg:ps-5 ">
+          <div className="gap-14 flex-row items-center justify-end mt-0 lg:relative absolute lg:flex hidden">
             <Link
               to="/"
               className={`${
@@ -105,8 +81,54 @@ const Navbar = () => {
             </Link>
           </div>
         </div>
-      </nav>
-    </header>
+      </div>
+      <div
+        className={`flex flex-col gap-5 p-5 z-20 bg-white absolute w-full duration-300 ${
+          hidden ? "top-20" : "-top-80"
+        }`}
+      >
+        <Link
+          to="/"
+          className={`${
+            location.pathname === "/" ? "active" : null
+          } duration-300 font-semibold text-xl text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className={`${
+            location.pathname === "/about" ? "active" : null
+          } duration-300 font-semibold text-xl text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}
+        >
+          About
+        </Link>
+        <Link
+          to="/services"
+          className={`${
+            location.pathname == "/services" ? "active" : null
+          } duration-300 font-semibold text-xl text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}
+        >
+          Services
+        </Link>
+        <Link
+          to="/blog"
+          className={`${
+            location.pathname == "/blog" ? "active" : null
+          } duration-300 font-semibold text-xl text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}
+        >
+          Blog
+        </Link>
+
+        <Link
+          type="button"
+          to="/contact"
+          className=" duration-300 py-3 px-4 inline-flex items-center gap-x-2 text-base font-semibold rounded-lg border border-transparent bg-[--main-color] text-white hover:bg-[--hover-color] disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 dark:text-gray-800"
+        >
+          Contact Me
+        </Link>
+      </div>
+    </div>
   );
 };
 
